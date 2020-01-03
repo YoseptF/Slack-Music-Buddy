50c46f9d18ff67f0d731a445efcdba600b617060
Yosept <yosept.flores@gmail.com>
2020-01-03 09:36:09 -0600

    youtube api was expired

--- lib/youtube.rb
+++ lib/youtube.rb
@@ -8,7 +8,6 @@ class Youtube
   # recieves a string and
   def self.youtube(search = 'beautifully')
     query = search.split(' ').join('%20')
-
     r = "q=#{query}&key=#{ENV['YOUTUBE_API']}"
     complete = 'https://www.googleapis.com/youtube/v3/search?part=snippet&' + r
     uri = URI.parse(complete)
@@ -29,8 +28,6 @@ class Youtube
       return "sorry I didn't find anything, can you be more specific :D?"
     end
 
-    puts "https://youtu.be/#{video_id}"
+    "https://youtu.be/#{video_id}"
   end
 end
-
-Youtube.youtube('sixteen tons')
